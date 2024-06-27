5e41be54d6aa781a847cff77f327bb1df654885f
emmcors <emmcors@hotmail.com>
2024-06-27 17:54:05 -0600

    agregamos funcionalidad al proyecto

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.header('Lanzar una moneda')
+car_data = pd.read_csv('vehicles_us.csv')
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
+st.header('Anuncios de Venta de Autos')
+
+
+
+
+build_histogram = st.checkbox('Construir un histograma')# crear una casilla de verificación
+build_disp = st.checkbox('Construir un gráfico de dispersión')# crear una casilla de verificación
+
+start_button = st.button('Crear')
+
+if start_button and build_histogram:
+    st.write('Numeros de Autos por Kilometraje')
+    fig1 = px.histogram(car_data, x="odometer") # crear un histograma
+    st.plotly_chart(fig1, use_container_width=True)
+elif start_button and build_disp:
+    st.write('Precios de los Autos en Relación con los Kilometros Recorridos')
+    fig2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    st.plotly_chart(fig2, use_container_width=True)
